6152f6686325caa6b6393b8036ef9a174b99b0e0
Giovanni Lenguito <giovanni16.gl@gmail.com>
2017-04-30 15:59:11 +0100

    - Fixes missing group name in stock list - Adds title to manifest in awaiting-materials

--- app/routes/stock.js
+++ app/routes/stock.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
+    this.store.findAll('group');
     return this.store.findAll('item');
   },
   setupController: function(controller, model) {
